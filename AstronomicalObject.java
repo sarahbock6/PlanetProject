8ac46b713b1a4662dbb75ed57c50c539cb180051
sarahbock6 <bocks@augsburg.edu>
2017-03-02 22:10:57 -0600

    Update AstronomicalObject.java

--- AstronomicalObject.java
+++ AstronomicalObject.java
@@ -1,38 +1,51 @@
 package inheritance;
-public class AstronomicalObject {
-    String name;
-    int size;
-    int weight;
-	
-  
-	public void Astronomical(String inname, int insize, int inweight){
-        name = inname;
-        size = insize;
-        weight = inweight;
 
-    }
-    public void setNAME(String namein){
-        name = namein;
-    }
-    public void setSIZE(int sizein){
-        size = sizein;
-    }
-    public void setWEIGHT(int weightin){
-        weight = weightin;
-    }
-    public String getNAME(){
-        return name;
-    }
-    public int getSIZE(){
-        return size;
-    }
-    public int getWEIGHT(){
-        return weight;
-    }
-    public String toString()
-    {
-        return "NAME: " + name + "" + "" + " SIZE: " + size + "" + " WEIGHT: " + weight;
-        
-      
-    }
+public class AstronomicalObject {
+		
+		String name;
+		int size;
+		int weight;
+
+		public AstronomicalObject(String name1, int size1, int weight1){
+
+		super();	
+		name = name1;
+		size = size1;
+		weight = weight1;
+
+	}
+
+	public void setNAME(String name1){
+		name = name1;
+	}
+
+	public void setSIZE(int size1){
+		size = size1;
+	}
+
+	public void setWEIGHT(int weight1){	
+		weight = weight1;
+	}
+	public String getNAME(){
+		return name;
+	
+	}
+
+	public int getSIZE(){
+		return size;
+
+	}
+
+	public int getWEIGHT(){
+		return weight;
+
+	}
+
+	public String toString()
+
+	{
+		return "NAME: " + name + "" + "" + " SIZE: " + size + "" + " WEIGHT: " + weight;
+	
+	}
+
 }
