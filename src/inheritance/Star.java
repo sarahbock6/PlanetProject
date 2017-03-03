4c46289280090502321582f2cf7c93c261fefad7
sarahbock6 <bocks@augsburg.edu>
2017-03-02 22:00:42 -0600

    Update Star.java

--- src/inheritance/Star.java
+++ src/inheritance/Star.java
@@ -1,82 +1,74 @@
 package inheritance;
 
-public class Star{
+public class Star extends AstronomicalObject {
 	
-	private String name;
 	private String location;
-    private String light;
-    int size;
-    int weight;
+    	private String light;
 
-    public Star(String name1, String location1, String light1, int size1, int weight1)
+	public Star(String name1, String location1, String light1, int size1, int weight1)	
 
     {
-    	
-        super();
+	super(name1, size1, weight1);
+	location = location1;
+	light = light1;
+		
+    }	
+
+	public void setNAME(String name1){
         name = name1;
-        location = location1;
-        light = light1;
-        size = size1;
-        weight = weight1;
-        
     }
 
-    public void setNAME(String name1){
-        name = name1;
+	public void setLOCATION(String location1){
+	location = location1;
         
     }
     
-    public void setLOCATION(String location1){
-        location = location1;
-        
-    }
-    
-    public void setLIGHT(String light1){
+	public void setLIGHT(String light1){
         light = light1;
         
     }
     
-    public void setSIZE(int size1){
+	public void setSIZE(int size1){
         size = size1;
         
     }
     
-    public void setWEIGHT(int weightin){
+	public void setWEIGHT(int weightin){
         weight = weightin;
         
     }
     
-    public String getNAME(){
+	public String getNAME(){
         return name;
         
     }
     
-    public String getLOCATION(){
+	public String getLOCATION(){
         return location;
     
     }
     
-    public String getLIGHT(){
+	public String getLIGHT(){
         return light;
     
     }
     
-    public int getSIZE(){
+	public int getSIZE(){
         return size;
     
     }
     
-    public int getWEIGHT(){
+	public int getWEIGHT(){
         return weight;
     
     }
     
-    public String toString()
+	public String toString()
     
     {
     	
-        return "NAME: " + name + "" + " LOCATION: "+ location + " LIGHT: " + light + "" + " SIZE: " + size + "" + " WEIGHT: " + weight;
-    
+		return "NAME: " + name + "" + " LOCATION: "+ location + " LIGHT: " + light + "" + " SIZE: " + size + "" + " WEIGHT: " + weight;
+   
     }
     
 }
